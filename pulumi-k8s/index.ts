e4bd6f00604f850eda4a0ccaac165a2c31ecee94
tt5 <2585803+tt5@users.noreply.github.com>
2022-07-17 12:43:15 +0000

    tekton

--- pulumi-k8s/index.ts
+++ pulumi-k8s/index.ts
@@ -1,6 +1,11 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as path from "path";
 
+const tekton = new k8s.yaml.ConfigFile("tekton", {
+  file: "https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml",
+});
+
 const manifests = new k8s.yaml.ConfigGroup("manifests", {
 	files: [ path.join("manifests", "*.yml") ],
-});
+}, {dependsOn: [tekton]});
+
--- pulumi/setupkubectl.sh
+++ pulumi/setupkubectl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-civo kubernetes config $(pulumi stack output clusterName --stack dev) --save > ./kubeconfig.civo
+civo kubernetes config $(pulumi stack output clusterName --stack cluster) --save > ./kubeconfig.civo
 export KUBECONFIG=$(pwd)/kubeconfig.civo
