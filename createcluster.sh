cdb3eacf280953db625018d26f6024ea005c72b4
tt5 <2585803+tt5@users.noreply.github.com>
2022-07-27 15:41:44 +0000

    no traefik

--- createcluster.sh
+++ createcluster.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-civo kubernetes create gitpod-test2 --size "g4s.kube.medium" --nodes 2 --wait --save --merge --region FRA1
+civo kubernetes create gitpod-test2 --size "g4s.kube.small" --nodes 3 --wait --save --merge --region FRA1 --remove-applications=Traefik
